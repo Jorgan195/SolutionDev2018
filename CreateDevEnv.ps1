a04dbf80cef3f65ecb943cd449592cb67fbd086e
Jeremy Organ <32566207+Jorgan195@users.noreply.github.com>
2018-09-03 10:54:16 +0100

    Initial Version
    Initial Version

---
+++ CreateDevEnv.ps1
@@ -0,0 +1,23 @@
+﻿
+# Updatehosts may require administrator permissions
+
+$mylicense = "c:\Navision\DC Nav 2018 withISV rights.flf"
+$imageName = "microsoft/dynamics-nav:2018"
+$sourceFolder = Join-Path $PSScriptRoot "Source"
+$containerName = Split-Path $PSScriptRoot -Leaf
+
+New-NavContainer -accept_eula `
+                 -containerName $containerName `
+                 -imageName $imageName `
+                 -memoryLimit 3G `
+                 -auth Windows `
+                 -licensefile $mylicense `
+                 -updateHosts `
+                 -includeCSide `
+                 -additionalParameters @("--volume ${sourceFolder}:c:\source",
+                                         #"--volume ${addInsFolder}:c:\run\Add-Ins",
+                                         #"--env CustomNavSettings=EnableTaskScheduler=true"
+                                         "-e locale=en-GB") 
+
+Import-DeltasToNavContainer -containerName $containerName -deltaFolder $sourceFolder -compile
+
---
+++ GetChanges.ps1
@@ -0,0 +1,4 @@
+﻿$sourceFolder = Join-Path $PSScriptRoot "Source"
+$containerName = Split-Path $PSScriptRoot -Leaf
+
+Export-ModifiedObjectsAsDeltas -containerName $containerName -deltaFolder $sourceFolder
